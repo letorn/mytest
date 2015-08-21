3100b81997edbd6bf02a0e598eff1715c7252422
Letorn <letorn@163.com>
2015-08-21 12:20:00 +0800

    init

---
+++ src/socket/SocketServer.java
@@ -0,0 +1,27 @@
+package socket;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketServer {
+
+	public static void main(String[] args) throws Exception {
+		ServerSocket server = new ServerSocket(5678);
+		Socket client = server.accept();
+		BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		PrintWriter out = new PrintWriter(client.getOutputStream());
+		while (true) {
+			String str = in.readLine();
+			System.out.println(str);
+			out.println("has receive....");
+			out.flush();
+			if (str.equals("end"))
+				break;
+		}
+		client.close();
+	}
+
+}
